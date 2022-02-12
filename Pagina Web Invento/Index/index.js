3fa1e5db1a9a85f33a2339d8021d83d718aefd4b
Pablo Paez <pablopaez2307@gmail.com>
2022-02-12 12:25:23 -0300

    primera carga de carpeta

---
+++ Pagina Web Invento/Index/index.js
@@ -0,0 +1,19 @@
+//Comienzo de JavaScript
+console.log("hola mundo en JavaScript")
+
+Nombre="Pepe"
+
+console.log("Mi nombre es",Nombre)
+
+
+// ES6
+let nombre1 = "Carla"
+const CURSO = "JS"
+const curso = "Javascript"
+
+console.log(nombre1, "esta en un curso de ", CURSO, "y ", curso)
+
+/*
+let nombre = prompt ("Ingrese su nombre")
+alert("Bienvenido " + nombre + " a su curso de JavaScript")             ALERTAS INPUT
+*/
